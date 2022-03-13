841fc3859b319d5dfb36599e898f7bec23f749b6
Novikov Artyom <kerambit.c@yandex.ru>
2022-03-13 16:31:30 +0300

    Добавьте файлы проекта.

---
+++ Sharp.RationalFraction/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Sharp.RationalFraction
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            RationalFraction rf = new RationalFraction(2, 5);
+            RationalFraction rf1 = new RationalFraction("2/6");
+
+            RationalFraction sum = rf + rf1;
+            RationalFraction diff = rf - rf1;
+            RationalFraction multi = rf * rf1;
+            RationalFraction div = rf / rf1;
+
+            div.SetWholePart();
+
+            Console.WriteLine(sum.ToString());
+            Console.WriteLine(diff.ToString());
+            Console.WriteLine(multi.ToString());
+            Console.WriteLine(div.ToString());
+
+        }
+
+    }
+}
---
+++ Sharp.RationalFraction/RationalFraction.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sharp.RationalFraction
+{
+    
+    class RationalFraction
+    {
+
+        public RationalFraction(int numerator, int denominator)
+        {
+
+            this.wholePart = 0;
+            this.numerator = numerator;
+            this.denominator = denominator;
+
+            if (this.denominator == 0)
+                throw new Exception("Знаменатель не может быть равен нулю");
+
+        }
+
+        public RationalFraction(string value)
+        {
+
+            string[] values = value.Split(new char[] { '/' }, StringSplitOptions.RemoveEmptyEntries);
+
+            if (values.Length != 2)
+                throw new Exception("Неверное число аргументов: " + values.Length);
+
+            int num;
+            int denom;
+
+            if (!int.TryParse(values[0], out num))
+                throw new Exception("Неверное значения числителя");
+            if (!int.TryParse(values[1], out denom))
+                throw new Exception("Неверное значение знаменателя");
+
+            this.numerator = num;
+            this.denominator = denom;
+
+        }
+
+        public void SetWholePart()
+        {
+
+            this.wholePart = this.numerator / this.denominator;
+            this.numerator %= this.denominator;
+
+        }
+
+        public static RationalFraction operator +(RationalFraction arg1, RationalFraction arg2)
+        {
+
+            if(arg1.denominator == arg2.denominator)
+                return new RationalFraction(arg1.numerator + arg2.numerator, arg1.denominator);
+
+            int arg1Changed = arg1.numerator * arg2.denominator;
+            int arg2Changed = arg2.numerator * arg1.denominator;
+
+            return new RationalFraction(arg1Changed + arg2Changed, arg1.denominator * arg2.denominator);
+
+        }
+
+        public static RationalFraction operator -(RationalFraction arg1, RationalFraction arg2)
+        {
+
+            if (arg1.denominator == arg2.denominator)
+                return new RationalFraction(arg1.numerator - arg2.numerator, arg1.denominator);
+
+            int arg1Changed = arg1.numerator * arg2.denominator;
+            int arg2Changed = arg2.numerator * arg1.denominator;
+
+            return new RationalFraction(arg1Changed - arg2Changed, arg1.denominator * arg2.denominator);
+
+        }
+
+        public static RationalFraction operator *(RationalFraction arg1, RationalFraction arg2)
+        {
+
+            return new RationalFraction(arg1.numerator * arg2.numerator, arg1.denominator * arg2.denominator);
+
+        }
+
+        public static RationalFraction operator /(RationalFraction arg1, RationalFraction arg2)
+        {
+
+            return new RationalFraction(arg1.numerator * arg2.denominator, arg1.denominator * arg2.numerator);
+
+        }
+
+        public override string ToString()
+        {
+
+            string result = "";
+
+            if (this.wholePart != 0)
+                result += this.wholePart.ToString();
+
+            result += $"({this.numerator}/{this.denominator})";
+
+            return result;
+
+        }
+
+        private int wholePart { get; set; }
+        private int denominator { get; set; }
+        private int numerator { get; set; }
+
+    }
+
+}
